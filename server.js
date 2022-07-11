1ef762d619d07723fac933eb20ec6429dad4aaf5
unknown <khhan1990@hanmail.net>
2022-07-11 22:24:13 +0900

    39번까지 완성

--- server.js
+++ server.js
@@ -2,51 +2,63 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 app.get("/products", (req, res) => {
-  const query = req.query;
-  console.log("QUERY :", query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 100000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "Jordan",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 15000,
-        seller: "Grab",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "createdAt", "imageUrl", "seller", "name"],
+  })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send("에러 발생");
+    });
 });
 
 app.get("/products/:id", (req, res) => {
   const { params } = req;
   const { id } = params;
-  res.send(`id는 ${id} 입니다.`);
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 조회에 에러가 발생 했습니다.");
+    });
 });
 
 app.post("/products", (req, res) => {
   const { body } = req;
-  const { description, name, seller, price } = body;
-  if (!name || !description || !seller || !price) {
+  const { description, name, seller, price, imageUrl } = body;
+  if (!name || !description || !seller || !price || !imageUrl) {
     res.status(400).send("모든 필드를 입력해 주세요.");
   }
   models.Product.create({
@@ -54,6 +66,7 @@ app.post("/products", (req, res) => {
     description,
     seller,
     price,
+    imageUrl,
   })
     .then((result) => {
       console.log("상품 생성 결과 : ", result);
@@ -65,6 +78,14 @@ app.post("/products", (req, res) => {
       console.error(err);
       res.status(400).send("상품 업로드에 문제가 발생했습니다.");
     });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
 });
 
 app.listen(port, () => {
